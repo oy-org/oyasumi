e074805237ecbd116d76439c41f748944ea13bc4
akinori-git <akinori.git@live.com>
2013-09-09 00:06:49 +0700

    Japanese characters added

--- src/oyasumi/Oyasumi.java
+++ src/oyasumi/Oyasumi.java
@@ -3,7 +3,7 @@ package oyasumi;
 public class Oyasumi {
 
     public static void main(String[] args) {
-        System.out.println("Oyasumi! お休みな!");
+        System.out.println("Oyasumi! お休みな!"); //Japanese characters added
         System.out.println("Good night!");
     }
 }
