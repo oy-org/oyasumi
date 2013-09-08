54f1bc23fb7da51cbddd1368256e2b3a8a355398
akinori-git <akinori.git@live.com>
2013-09-08 23:33:19 +0700

    remove notes

--- src/oyasumi/Oyasumi.java
+++ src/oyasumi/Oyasumi.java
@@ -1,20 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package oyasumi;
 
-/**
- *
- * @author duc
- */
 public class Oyasumi {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Oyasumi!");
         System.out.println("Good night!");
     }
